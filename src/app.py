ea4ec26b2bc0ec762b9461d7287d263635e5c7a8
Hiimsuso <suso3543@gmail.com>
2025-04-14 19:58:17 +0200

    1

--- src/app.py
+++ src/app.py
@@ -1,4 +1,46 @@
-from utils import db_connect
-engine = db_connect()
+from pickle import load
+import streamlit as st
 
-# your code here
+model = load(open("/Users/jesus/Desktop/4geeks/4geeks -  streamlit/jesus-cuenca-STREAMLIT-machine-learning-python-template/models/best_model.pkl", "rb"))
+
+st.title("Random forest")
+
+embarazos = st.number_input(
+    "Indica un valor", value=None, placeholder="Indica un número..."
+)
+st.write("El número actual es: ", embarazos)
+
+glucosa = st.number_input(
+    "Indica un valor", value=None, placeholder="Indica un número..."
+)
+st.write("El número actual es: ", glucosa)
+
+Presión_sanguínea = st.number_input(
+    "Indica un valor", value=None, placeholder="Indica un número..."
+)
+st.write("El número actual es: ", Presión_sanguínea)
+
+Grosor_piel = st.number_input(
+    "Indica un valor", value=None, placeholder="Indica un número..."
+)
+st.write("El número actual es: ", Grosor_piel)
+
+Insulina = st.number_input(
+    "Indica un valor", value=None, placeholder="Indica un número..."
+)
+st.write("El número actual es: ", Insulina)
+
+BMI = st.number_input(
+    "Indica un valor", value=None, placeholder="Indica un número..."
+)
+st.write("El número actual es: ", BMI)
+
+Diabetes = st.number_input(
+    "Indica un valor", value=None, placeholder="Indica un número..."
+)
+st.write("El número actual es: ", Diabetes)
+
+Edad = st.number_input(
+    "Indica un valor", value=None, placeholder="Indica un número..."
+)
+st.write("El número actual es: ", Edad)
